5bc3cc61d3e5040fdafcf8bc4b1e7188ea04ee3c
mohaimenur1 <mmrahmanrahi@gmail.com>
2023-07-19 15:45:19 +0600

    add to cart slice added

---
+++ frontend/src/slices/cartSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+import cartSliceReducer from "./slices/cartSlice";
+
+const initialState = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : { cartItems: [], shippingAddress: {}, paymentMethod: "PayPal" };
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    cart: cartSliceReducer,
+  },
+});
+
+export const {
+  addToCart,
+  removeFromCart,
+  saveShippingAddress,
+  savePaymentMethod,
+  clearCartItems,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
