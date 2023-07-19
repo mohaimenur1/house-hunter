37cf611437bc27daccd6348ebddbb2a6532b90fc
mohaimenur1 <mmrahmanrahi@gmail.com>
2023-07-19 15:15:58 +0600

    redux toolkit for state management

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,8 +8,11 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import HomePage from "./pages/HomePage.jsx";
 import SingleHouse from "./pages/SingleHouse.jsx";
+import store from "./store.js";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -22,6 +25,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
